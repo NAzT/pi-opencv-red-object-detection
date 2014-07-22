2e931c829ba8c5b19c1ce000aadd44bfb0fa2f66
Nat <nat.wrw@gmail.com>
2014-07-22 17:27:52 +0700

    add build file

---
+++ build/CMakeFiles/mmal_opencv_window_demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/mmal_opencv_window_demo.dir/opencv_window_demo.c.o"
+  "mmal_opencv_window_demo.pdb"
+  "mmal_opencv_window_demo"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/mmal_opencv_window_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/nat.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+  fprintf(stdout, "HELLO"); 
+ return 0;
+}
